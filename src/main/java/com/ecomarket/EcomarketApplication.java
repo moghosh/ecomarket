6852bb00e46507a67d6279d3b504d101f88bb2ca
Moushumi Ghosh <moghosh@expedia.com>
2018-01-03 18:57:23 -0600

    Initial commit for project

---
+++ src/main/java/com/ecomarket/EcomarketApplication.java
@@ -0,0 +1,12 @@
+package com.ecomarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomarketApplication.class, args);
+	}
+}
