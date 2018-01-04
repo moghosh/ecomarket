c05e1533c831292744d0136e6a0b61718a62be07
Moushumi Ghosh <moghosh@expedia.com>
2018-01-03 19:22:44 -0600

    Working helloworld

---
+++ src/main/java/com/ecomarket/controllers/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.ecomarket.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by moghosh on 1/3/18.
+ */
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String sayHello(){
+        return "index";
+    }
+}
