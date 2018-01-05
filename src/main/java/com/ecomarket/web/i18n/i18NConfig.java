341c0d8e7dada0a22cd5bc5063996fdc5318a564
Moushumi Ghosh <moghosh@expedia.com>
2018-01-05 13:41:47 -0600

    Added a reloadable message resource bundle

---
+++ src/main/java/com/ecomarket/web/i18n/i18NConfig.java
@@ -0,0 +1,24 @@
+package com.ecomarket.web.i18n;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+/**
+ * Created by moghosh on 1/5/18.
+ */
+@Configuration
+public class i18NConfig {
+
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource(){
+        ReloadableResourceBundleMessageSource reloadableResourceBundleMessageSource = new ReloadableResourceBundleMessageSource();
+        reloadableResourceBundleMessageSource.setBasename("classpath:i18n/messages");
+
+        //Checks for new messages every 30 minutes.
+        reloadableResourceBundleMessageSource.setCacheSeconds(1800);
+        return reloadableResourceBundleMessageSource;
+
+    }
+
+}
